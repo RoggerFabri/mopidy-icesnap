5932dfcdcd7db535474288d1d0a37191dbf84336
RoggerFabri <rogger.rfs@gmail.com>
2020-06-04 20:14:36 +0100

    small fixes

--- icecast/start.sh
+++ icecast/start.sh
@@ -5,19 +5,19 @@ env
 set -x
 
 if [ -n "$ICECAST_SOURCE_PASSWORD" ]; then
-    sed -i "s/<source-password>[^<]*<\/source-password>/<source-password>$ICECAST_SOURCE_PASSWORD<\/source-password>/g" /usr/share/icecast2/icecast.xml
+    sed -i "s/<source-password>[^<]*<\/source-password>/<source-password>$ICECAST_SOURCE_PASSWORD<\/source-password>/g" /etc/icecast2/icecast.xml
 fi
 
 if [ -n "$ICECAST_RELAY_PASSWORD" ]; then
-    sed -i "s/<relay-password>[^<]*<\/relay-password>/<relay-password>$ICECAST_RELAY_PASSWORD<\/relay-password>/g" /usr/share/icecast2/icecast.xml
+    sed -i "s/<relay-password>[^<]*<\/relay-password>/<relay-password>$ICECAST_RELAY_PASSWORD<\/relay-password>/g" /etc/icecast2/icecast.xml
 fi
 
 if [ -n "$ICECAST_ADMIN_PASSWORD" ]; then
-    sed -i "s/<admin-password>[^<]*<\/admin-password>/<admin-password>$ICECAST_ADMIN_PASSWORD<\/admin-password>/g" /usr/share/icecast2/icecast.xml
+    sed -i "s/<admin-password>[^<]*<\/admin-password>/<admin-password>$ICECAST_ADMIN_PASSWORD<\/admin-password>/g" /etc/icecast2/icecast.xml
 fi
 
 if [ -n "$ICECAST_PASSWORD" ]; then
-    sed -i "s/<password>[^<]*<\/password>/<password>$ICECAST_PASSWORD<\/password>/g" /usr/share/icecast2/icecast.xml
+    sed -i "s/<password>[^<]*<\/password>/<password>$ICECAST_PASSWORD<\/password>/g" /etc/icecast2/icecast.xml
 fi
 
-sudo -Eu icecast2 icecast2 -n -c /usr/share/icecast2/icecast.xml
+sudo -Eu icecast2 icecast2 -n -c /etc/icecast2/icecast.xml
