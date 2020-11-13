c8d3bf659fd59de6e49f9b64d21ff35034809b16
RoggerFabri <rogger.rfs@gmail.com>
2020-11-13 12:20:46 +0000

    more flexibility on stream manager and snapserver

--- stream-manager/stream_manager.py
+++ stream-manager/stream_manager.py
@@ -2,9 +2,12 @@ import os
 import asyncio
 import snapcast.control
 import sys
+
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
-server = loop.run_until_complete(snapcast.control.create_server(loop, 'SNAPSERVER', 1705))
+serverIp = os.environ.get('SNAPSERVER_IP')
+serverPort = os.environ.get('SNAPSERVER_PORT')
+server = loop.run_until_complete(snapcast.control.create_server(loop, serverIp, serverPort))
 
 def on_stream_update(data):
     for stream in server.streams:
