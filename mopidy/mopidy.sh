9293fb6c94b6d2dbb460c10313ad7d9f0ea91297
RoggerFabri <rogger.rfs@gmail.com>
2022-03-18 11:53:34 +0000

    ref: mopidy Dockerfile
    Major refactor on mopidy Dockerfile to make it more simple

--- mopidy/mopidy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Copy config if it does not already exist
-if [ ! -f /mopidy/mopidy.conf ]; then
-    cp /mopidy_default.conf /mopidy/mopidy.conf
-fi
-
-exec mopidy --config /mopidy/mopidy.conf
