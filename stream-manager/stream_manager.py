e3204e9417ffd9912ec198ce40d88928030eda39
RoggerFabri <rogger.rfs@gmail.com>
2022-05-21 11:43:05 +0100

    Major refactor of stream_manager.py
    Python script was broken, needed a proper fix :)

--- stream-manager/stream_manager.py
+++ stream-manager/stream_manager.py
@@ -2,44 +2,58 @@ import os
 import asyncio
 import snapcast.control
 import sys
+import time
+import logging
 
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%d-%m-%Y %H:%M:%S')
+
+loop = asyncio.get_event_loop()
 
 serverIp = os.environ.get('SNAPSERVER_IP')
 serverPort = os.environ.get('SNAPSERVER_PORT')
 
-print(f'SNAPSERVER_IP: {serverIp}')
-print(f'SNAPSERVER_PORT: {serverPort}')
+logging.info(f'SNAPSERVER_IP: {serverIp}')
+logging.info(f'SNAPSERVER_PORT: {serverPort}')
 
 if serverIp is None:
-    print('ENV|SNAPSERVER_IP not provided, exiting...')
+    logging.error('ENV|SNAPSERVER_IP not provided, exiting...')
     sys.exit()
 if serverPort is None:
-    print('ENV|SNAPSERVER_PORT not provided, exiting...')
+    logging.error('ENV|SNAPSERVER_PORT not provided, exiting...')
     sys.exit()
 
 try:
-    print('Connecting to server...')
+    logging.info('Connecting to server...')
     server = loop.run_until_complete(snapcast.control.create_server(loop, serverIp, serverPort))
 except Exception as e:
-    print('Cannot connect to server, check settings and try again.')
-    print(e)
+    logging.error('Cannot connect to server, check settings and try again.')
+    logging.exception(e)
     sys.exit()
 
-def on_stream_update(data):
+def get_playing_stream():
     for stream in server.streams:
         if stream.status == 'playing':
-            for group in server.groups:
-                group.set_stream(stream.identifier)
-                print(f'Currently Playing Stream: {stream.identifier}')
+            return stream.identifier
+
+def update_stream_in_groups(stream):
+    logging.info('Updating playing stream to: %s', stream)
+    for group in server.groups:
+        group.set_stream(stream)
 
 try:
-    for stream in server.streams:
-        stream.set_callback(on_stream_update)
-
-    loop.run_forever()
+    stream = ""
+    while True:
+        time.sleep(1)
+        playing_stream = get_playing_stream()
+        if stream != playing_stream:
+            stream = playing_stream
+            logging.info('Currently playing stream: %s', stream)
+            update_stream_in_groups(stream)
+            
 except Exception as e:
-    print('Unhandled exception, check the logs for details.')
-    print(e)
+    logging.error('Unhandled exception, check the logs for details.')
+    logging.exception(e)
     sys.exit()
