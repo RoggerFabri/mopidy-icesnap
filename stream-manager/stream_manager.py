58db0f42c16e8623691c40652ebc28620b8e2924
RoggerFabri <rogger.rfs@gmail.com>
2022-05-21 13:30:15 +0100

    Proper usage of asyncio.loop

--- stream-manager/stream_manager.py
+++ stream-manager/stream_manager.py
@@ -10,8 +10,6 @@ logging.basicConfig(
     level=logging.INFO,
     datefmt='%d-%m-%Y %H:%M:%S')
 
-loop = asyncio.get_event_loop()
-
 serverIp = os.environ.get('SNAPSERVER_IP')
 serverPort = os.environ.get('SNAPSERVER_PORT')
 
@@ -27,32 +25,25 @@ if serverPort is None:
 
 try:
     logging.info('Connecting to server...')
+    loop = asyncio.get_event_loop()
     server = loop.run_until_complete(snapcast.control.create_server(loop, serverIp, serverPort))
 except Exception as e:
     logging.error('Cannot connect to server, check settings and try again.')
     logging.exception(e)
     sys.exit()
 
-def get_playing_stream():
+def on_stream_update(data):
     for stream in server.streams:
         if stream.status == 'playing':
-            return stream.identifier
-
-def update_stream_in_groups(stream):
-    logging.info('Updating playing stream to: %s', stream)
-    for group in server.groups:
-        group.set_stream(stream)
+            for group in server.groups:
+                loop.create_task(group.set_stream(stream.identifier))
+                logging.info(f'Currently Playing Stream: {stream.identifier}')
 
 try:
-    stream = ""
-    while True:
-        time.sleep(1)
-        playing_stream = get_playing_stream()
-        if stream != playing_stream:
-            stream = playing_stream
-            logging.info('Currently playing stream: %s', stream)
-            update_stream_in_groups(stream)
-            
+    for stream in server.streams:
+        stream.set_callback(on_stream_update)
+
+    loop.run_forever()
 except Exception as e:
     logging.error('Unhandled exception, check the logs for details.')
     logging.exception(e)
